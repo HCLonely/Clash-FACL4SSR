a6c2fbc39bbb73ae67bcd516c5e91ed592940a8d
HCLonely <h1606051253@gmail.com>
2024-10-15 10:49:13 +0800

    Update at 2024-10-15 10:49:13

--- update.js
+++ update.js
@@ -4,7 +4,16 @@ const fs = require('fs');
 axios.get('https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full.ini').then((response) => {
   const arr = response.data.split('\n');
   const newText = arr.map((value) => {
-    if (value.includes('广告拦截') || value.includes('应用净化')) {
+    if (value.includes('广告拦截')) {
+      if (value.includes('ruleset=')) {
+        return 'ruleset=🎯 全球直连,https://raw.githubusercontent.com/HCLonely/Clash-FACL4SSR/refs/heads/main/Direct.list';
+      }
+      return null;
+    }
+    if (value.includes('应用净化')) {
+      if (value.includes('ruleset=')) {
+        return 'ruleset=🚀 节点选择,https://raw.githubusercontent.com/HCLonely/Clash-FACL4SSR/refs/heads/main/Proxy.list';
+      }
       return null;
     }
     return value;
